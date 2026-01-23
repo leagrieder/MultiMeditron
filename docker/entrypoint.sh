aeabfb0b1e3ba26d434f939624f44e95bcc612d3
Michael Zhang <57410827+MichelDucartier@users.noreply.github.com>
2026-01-23 15:39:13 +0100

    Add possibility to spawn as a user in Docker image (#41)
    * Add Dockerfile for user
    
    * insh
    
    * Make default userID and groupID 0

---
+++ docker/entrypoint.sh
@@ -0,0 +1,8 @@
+USER_ID=${USER_ID:0}
+GROUP_ID=${GROUP_ID:0}
+USERNAME=${USERNAME:root}
+
+groupadd -f -g $GROUP_ID $USERNAME || true
+id -u $USERNAME &>/dev/null || useradd -f -m -u $USER_ID -g $GROUP_ID $USERNAME || true
+
+exec gosu $USER_ID:$GROUP_ID "$@"
