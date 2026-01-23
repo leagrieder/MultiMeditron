f4c1f6a5c997f6f3fceb924c7770bc902e54f850
MichelDucartier <m.zhang2490@gmail.com>
2026-01-23 15:53:35 +0100

    Fix entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,6 +3,6 @@ GROUP_ID=${GROUP_ID:0}
 USERNAME=${USERNAME:root}
 
 groupadd -f -g $GROUP_ID $USERNAME || true
-id -u $USERNAME &>/dev/null || useradd -f -m -u $USER_ID -g $GROUP_ID $USERNAME || true
+id -u $USERNAME &>/dev/null || useradd -m -u $USER_ID -g $GROUP_ID $USERNAME || true
 
 exec gosu $USER_ID:$GROUP_ID "$@"
